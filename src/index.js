9ac7fd2d93838bd771256d89bdd22bbfef0056bc
tHALL3000 <80265634+tHALL3000@users.noreply.github.com>
2021-06-27 05:51:17 -0600

    Create loop over array chunk of code using {users.map(

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ScotchInfoBar from "./ScotchInfoBar";
  * Our data comes from users-data.js
  * -----------------------------
  */
-import users from "./users-data";
+import users from "./users-data.js";
 
 /**
  * Our React component where we display data
@@ -23,8 +23,13 @@ function App() {
       {/* Iterate over imported array in userData */}
       <div className="users">
         display data here
-        {/* Display each data in array in a card */}
-        {/* Each card must have a 'key' attribute */}
+        {users.map((user, index) => (
+          <div key={index}>
+            <h3> {user.name}</h3>
+            <p> {user.location} </p>
+            <p> {user.car} </p>
+          </div>
+        ))}
       </div>
       <ScotchInfoBar />
     </div>
